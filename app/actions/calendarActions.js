bdabd4123f93aff78f76f4a8fef6ceaf3c9b7a9b
肖玉萃 <xiaoyc@didikon.com>
2016-07-20 10:08:52 +0800

    delete some file

--- app/actions/calendarActions.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by mxyue on 16/6/20.
- */
-
-export const CURRENT_TODOS = 'CURRENT_TODOS';
-
-export function getCurrentTodos(){
-    return {type: CURRENT_TODOS}
-}
--- app/reducers/calendarReducers.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by mxyue on 16/6/20.
- */
-import {
-    CURRENT_TODOS
-} from '../actions/calendarActions'
